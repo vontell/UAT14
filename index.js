0b9b6db46c5ed456139620d13ac35825909b3e2b
Aaron Vontell <aaron@vontech.org>
2023-10-06 16:10:11 +0000

    Time limit the bot

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 export function configureBot(rg) {
-    rg.characterConfig = {
-      speed: 4000
-    };
+  rg.characterConfig = {
+    speed: 4000
+  };
+}
+
+var startTime = null
+
+export async function processTick(rg) {
+
+  if (!startTime) startTime = new Date().getTime();
+
+  let powerUps = rg.findEntities("PowerUp")
+  if (powerUps) {
+    rg.performAction("MoveToPosition", powerUps[0].position)
   }
-  
-  export async function processTick(rg) {
-  
-    let powerUps = rg.findEntities("PowerUp")
-    if (powerUps) {
-      rg.performAction("MoveToPosition", powerUps[0].position)
-    }
-  
+
+  if (new Date().getTime() - startTime > 5000) {
+    // Now try to finish
+    rg.complete();
   }
+
+}
